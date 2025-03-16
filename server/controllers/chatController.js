50a1288d60ced92b77e4e11088eb35910d902b18
AllisonIves <114875124+AllisonIves@users.noreply.github.com>
2025-03-16 13:22:57 -0400

    Maximum ten users implemented

--- server/controllers/chatController.js
+++ server/controllers/chatController.js
@@ -1,34 +1,37 @@
 module.exports = function(io) {
-
-    let users = []; // Array to track the users
+    const MAX_USERS = 10; //Maximum users
+    let users = []; //Array to track the users
 
     io.on('connection', (socket) => {
         console.log('A user connected:', socket.id);
     
-        // Send the list of users to the new client
+        //Send the list of users to the new user
         socket.emit('userList', users);
     
-        // Handle new user joining
+        //Handle new user joining
         socket.on('setUsername', (username) => {
-            if (!users.includes(username)) {
-                users.push(username);  // Add the new user to the list
-                socket.username = username; // Store the username in the socket object
+            if (users.length < MAX_USERS) {
+                if (!users.includes(username)) {
+                    users.push(username);  //Add new jser
+                    socket.username = username;
+                }
+                //Update userlist for all users
+                io.emit('userList', users);
+            } else {
+                socket.emit('userLimitReached', 'The chat is full.'); //Temporary message to be replaced when new chat functionality added
+                socket.disconnect();
             }
-            
-            // Notify all clients about the updated user list
-            io.emit('userList', users);
         });
-    
-        // Handle message sending
+        //Handle message sending
         socket.on('sendMessage', (data) => {
             io.emit('receiveMessage', data);
         });
     
-        // Handle user disconnecting
+        //Handle user disconnecting
         socket.on('disconnect', () => {
-            // Remove the username from the list when a user disconnects
+            //Remove the username from the list
             users = users.filter(user => user !== socket.username);
-            io.emit('userList', users); // Update the user list on all clients
+            io.emit('userList', users); //Update userlist for all users
             console.log('User disconnected:', socket.id);
         });
     });
