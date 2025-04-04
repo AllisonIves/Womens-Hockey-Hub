89b6d2a853f9680f6d21bee7934d412e8fda46d0
Derek Leduc <dleduc1@my.centennialcollege.ca>
2025-04-04 12:58:18 -0400

    Save work before rebase

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -21,7 +21,7 @@ router.post('/:postId/reply', createReply);
 router.delete('/id/:id', deletePostById);
 router.delete('/', deleteAllPosts);
 router.get('/categories', getAllCategories);
-router.put('/id/:postID', updateForumPost);
+router.put('/id/:postId', updateForumPost);
 router.put('/id/:postId/:replyId', updateReply);
 
 module.exports = router;
